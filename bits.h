1ffb59757690c4943c25bfbe776aca0c1072b5c0
David Chan <40187568+davidchannn@users.noreply.github.com>
2024-03-15 19:08:39 -0700

    Create bits.h

---
+++ bits.h
@@ -0,0 +1,24 @@
+/*
+ * bits.h
+ *
+ *  Created on: Jan 30, 2024
+ *      Author: davidchan
+ */
+
+// this file was in pretty much every project
+
+#ifndef BITS_H_
+#define BITS_H_
+
+#define BIT_0 0x01
+#define BIT_1 0x02
+#define BIT_2 0x04
+#define BIT_3 0x08
+#define BIT_4 0x10
+#define BIT_5 0x20
+#define BIT_6 0x40
+#define BIT_7 0x80
+#define BIT_8 0x100
+#define BIT_9 0x200
+
+#endif /* BITS_H_ */
